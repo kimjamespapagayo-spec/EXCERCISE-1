497e4a8a8b6c443896e7452607574563b13e54bd
kimjamespapagayo-spec <kimjames.papagayo@hcdc.edu.ph>
2025-09-05 20:38:33 +0800

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,29 @@
+let users = [
+  { id: 1, name: "Kristian" },
+  { id: 2, name: "Malingin" }
+];
+
+let names = users.map(user => user.name);
+console.log(names); // Result: ["Kristian", "Malingin"]
+
+let people = [
+  { name: "Kristian", age: 16 },
+  { name: "Malingin", age: 18 },
+  { name: "Spike", age: 22 }
+];
+
+let adults = people.filter(person => person.age >= 18);
+console.log(adults); // Result: [{name:"Malingin", age:18}, {name:"Spike", age:22}]
+
+let people = [
+  { name: "Kristian", age: 25 },
+  { name: "Malingin", age: 32 },
+  { name: "Charlie", age: 29 }
+];
+
+let oldestPerson = people.reduce((oldest, person) => {
+  return person.age > oldest.age ? person : oldest;
+}, people[0]);
+
+console.log(oldestPerson); // Result: { name: "Malingin", age: 32 }
+
